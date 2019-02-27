c1577e7d5ecbed31f19e92cb18d1e1f69e8d6572
Cam Williams <cam@Cams-MacBook-Pro.local>
2019-02-27 14:50:49 -0800

    alex css updates

---
+++ static/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+//get height of right div
+
+var eRightHt = $(".section_splash").outerHeight();
+var eRighttrue = eRightHt + 160
+// apply height to image in leftdiv
+
+$(".splash").css("height",eRighttrue);
+
+var $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+    // Do something else, like open/close menu
+  });
+
+});
