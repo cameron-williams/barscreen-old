d95f33f1e75d12ce8c1930d6c148375155765d9f
Cam Williams <cam@Cams-MacBook-Pro.local>
2019-03-13 19:01:42 -0700

    asdf

--- views/base.py
+++ views/base.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from forms.contact import ContactForm
+from services.google import Gmail
 
 base = Blueprint('base', __name__, static_folder='../static')
 
@@ -13,11 +15,25 @@ def about():
     return render_template("about.html")
 
 
-@base.route("/contact", methods=["POST","GET"])
+@base.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    form = ContactForm()
+    if request.method == "POST" and form.validate_on_submit():
+        g = Gmail(delegated_user="cam@barscreen.tv")
+        msg = """
+            New Contact Form Submission:
+            Name: {}
+            Email: {}
+            Message: {}""".format(
+            form.name.data,
+            form.email.data,
+            form.message.data,
+        )
+        g.send_email(to="info@barscreen.tv", subject="New Contact Submission", body=msg)
+
+    return render_template("contact.html", form=form)
 
 
-@base.route("/signup", methods=["POST","GET"])
+@base.route("/signup", methods=["POST", "GET"])
 def signup():
     return render_template("signup.html")
