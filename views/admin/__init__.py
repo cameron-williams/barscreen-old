02e10b4dd13c3ce5184611f9b3b555c54f1776fa
Alex Thain <Alex-Gravity@Alexs-MacBook-Pro.local>
2019-03-18 23:16:26 -0700

    channel and styles

--- views/admin/__init__.py
+++ views/admin/__init__.py
@@ -13,3 +13,22 @@ def index():
 @admin.route("/user")
 def user():
     return render_template("admin/user.html")
+
+@admin.route("/channels")
+def channels():
+    return render_template("admin/channels.html")
+
+@admin.route("/addchannel")
+def addchannel():
+    form = NewchannelForm()
+    if request.method == "POST" and form.validate_on_submit():
+        channel = Channels(
+            channel_name=form.channel_name.data,
+            category=form.category.data,
+            description=form.description.data,
+            channel_img=form.channel_img.data,
+        )
+        db.session.add(channel)
+        db.session.commit()
+        flash("Successfully completed.")
+    return render_template("admin/addchannel.html", form=form)
