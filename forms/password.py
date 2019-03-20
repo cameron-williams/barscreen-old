f2a727632be84c0b9068b1de60d1539f1adb3a3f
Alex Thain <Alex-Gravity@Alexs-MacBook-Pro.local>
2019-03-20 11:23:27 -0700

    password confirm 2

--- forms/password.py
+++ forms/password.py
@@ -5,5 +5,6 @@ from wtforms.validators import DataRequired
 
 class CreatePassword(FlaskForm):
     token = HiddenField("Submit Token", validators=[DataRequired()])
-    password = PasswordField("New Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()],validators.EqualTo('confirm', message='Passwords must match'))
+    confirm = PasswordField('Repeat Password')
     submit = SubmitField("Submit")
