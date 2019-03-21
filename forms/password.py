4d2bb8178b961d57b7ddce9f22e14da409249552
Cam Williams <cam@Cams-MacBook-Pro.local>
2019-03-21 12:24:57 -0700

    fix form

--- forms/password.py
+++ forms/password.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField, HiddenField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 
 class CreatePassword(FlaskForm):
     token = HiddenField("Submit Token", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()],validators.EqualTo('confirm', message='Passwords must match'))
+    password = PasswordField("Password",
+                             validators=[DataRequired(), EqualTo(fieldname='confirm', message='Passwords must match')])
     confirm = PasswordField('Repeat Password')
     submit = SubmitField("Submit")
