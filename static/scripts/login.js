2f22818abd2dc2346ab5bf4c22b07124f4897ccf
Cam <cam@camwilliams.ca>
2019-06-18 16:51:57 -0700

    add err printing to forgot pw

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         data: JSON.stringify({"email": email}),
         dataType: "json",
         contentType: "application/json",
-        error: function(errMsg){alert("Sorry: " + errMsg)},
+        error: function(jqXHR, exception){alert("Sorry: " + exception); console.log(jqXHR); console.log(exception);},
     });
     $(document).find(".alternate2").delay(250).fadeIn(250)
   });
